18e1e9ea4186be0b97d182f94718b3f657ef3f31
patrickleon710 <134448809+patrickleon710@users.noreply.github.com>
2025-12-10 14:14:45 +0300

    add init and profiles files, add gitignore

---
+++ Flaskr/profiles.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        # Here you would add logic to verify username and password
+        return f"Logged in as: {username}"
+    return '''
+        <form method="post">
+            Username: <input type="text" name="username"><br>
+            Password: <input type="password" name="password"><br>
+            <input type="submit" value="Login">
+        </form>
+    '''
