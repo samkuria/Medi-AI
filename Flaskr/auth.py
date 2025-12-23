73ccc47e3ef4d219584caa3f707b7ef1c0cdd4fe
patrickleon710 <134448809+patrickleon710@users.noreply.github.com>
2025-12-23 07:25:25 +0300

    Add require-login function

--- Flaskr/auth.py
+++ Flaskr/auth.py
@@ -1,3 +1,4 @@
+import functools
 from flask import (
     Blueprint, flash, g, render_template, request, redirect, url_for, session
 )
@@ -10,25 +11,28 @@ bp = Blueprint("auth", __name__, url_prefix="/auth")
 @bp.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        username = request.form["username"]
+        email = request.form["email"]
         password = request.form["password"]
+        password_repeat = request.form["password-repeat"]
         db = get_db()
         error = None
 
-        if not username:
-            error = "Username is required!"
+        if not email:
+            error = "Email is required!"
         elif not password:
             error = "Password is required!"
+        elif password != password_repeat:
+            error = "Passwords do not match!"
 
         if error is None:
             try:
                 db.execute(
-                    "INSERT into user (username, password) VALUES (?,?)",
-                    (username, generate_password_hash(password))
+                    "INSERT into user (email, password) VALUES (?,?)",
+                    (email, generate_password_hash(password))
                 )
                 db.commit()
             except db.IntegrityError:
-                error = f"Username {username} already in use!"
+                error = f"Email {email} already in use!"
             else:
                 return redirect(url_for("auth.login"))
             
@@ -40,17 +44,17 @@ def register():
 @bp.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        username = request.form["username"]
+        email = request.form["email"]
         password = request.form["password"]
         db = get_db()
         error = None
 
         user = db.execute(
-            "SELECT * FROM user WHERE username = ?", (username,)
+            "SELECT * FROM user WHERE email = ?", (email,)
         ).fetchone()
 
         if user is None:
-            error = "Incorrect username!"
+            error = "Incorrect email!"
         elif not check_password_hash(user["password"], password):
             error = "Incorrect password!"
 
@@ -76,6 +80,14 @@ def load_logged_in_user():
             "SELECT * FROM user WHERE id = ?", (user_id)
         ).fetchone()
 
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+        return view(**kwargs)
+    return wrapped_view
     
 @bp.route("/logout")
 def logout():
