5dba707a923b1087f38ef9b2a371fb356c1619ca
Samuel_Kuria <145743807+samkuria@users.noreply.github.com>
2026-01-13 13:05:27 +0300

    Implement routing for Chatbot and Dashboard pages

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Chatbot from "./pages/Chatbot/Chatbot";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
-  return <Chatbot />;
+  return (
+    
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/chat" element={<Chatbot />} />
+    </Routes>
+    
+  )
 }
 
 export default App;
